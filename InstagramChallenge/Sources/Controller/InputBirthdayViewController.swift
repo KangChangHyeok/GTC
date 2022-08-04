be5928f2766f28cfa8b9a431903f285f2cbfc6af
KangChangHyeok <dpkyle97@gmail.com>
2022-08-04 23:16:16 +0900

    로직 구현
    - 페이지 4-5 로직 구현 완료

--- InstagramChallenge/Sources/Controller/InputBirthdayViewController.swift
+++ InstagramChallenge/Sources/Controller/InputBirthdayViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class InputBirthdayViewController: UIViewController {
 
+    @IBOutlet weak var userDateofBirth: UIDatePicker!
+    @IBOutlet weak var userBirthDay: UILabel! {
+        didSet {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy년 MM월 dd일"
+            userBirthDay.text = formatter.string(from: Date())
+            userBirthDay.textColor = .lightGray
+        }
+    }
+    @IBOutlet weak var userAge: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -16,5 +27,28 @@ class InputBirthdayViewController: UIViewController {
         let acceptViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AcceptViewController") as! AcceptViewController
         acceptViewController.modalPresentationStyle = .fullScreen
         present(acceptViewController, animated: false)
+    }
+    @IBAction func changeUserBirthDay(_ sender: UIDatePicker) {
+        userBirthDay.textColor = .black
+        // 유저 나이 구하기
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "yyyy"
+        let userBirth = Int(dateformatter.string(from: sender.date))!
+        let now = Int(dateformatter.string(from: Date()))!
+        userAge.text = (now - userBirth + 1).description + "세"
+        // 유저 생일 표시하기
+        dateformatter.dateFormat = "yyyy년 MM월 dd일"
+        userBirthDay.text = dateformatter.string(from: sender.date)
+        dateformatter.dateFormat = "yyyy.MM.dd"
+        UserSignUpInfo.shared.userBirthday = dateformatter.string(from: sender.date)
+        // 오늘 이후 날짜 선택시 버튼 비활성화
+        dateformatter.dateFormat = "yyyyMMdd"
+        let nowYear = Int(dateformatter.string(from: Date()))!
+        let selectYear = Int(dateformatter.string(from: sender.date))!
+        if nowYear - selectYear < 0 {
+            nextButton.isEnabled = false
+            nextButton.alpha = 0.5
+            userAge.text = ""
+        }
     }
 }
