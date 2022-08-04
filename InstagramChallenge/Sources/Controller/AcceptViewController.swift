1e512cd865e1a70c73e7068197c2884e00c038f8
KangChangHyeok <dpkyle97@gmail.com>
2022-08-04 23:57:26 +0900

    로직 구현
    - 페이지 4-6 로직 구현완료(링크 이동 기능 제외)

--- InstagramChallenge/Sources/Controller/AcceptViewController.swift
+++ InstagramChallenge/Sources/Controller/AcceptViewController.swift
@@ -11,13 +11,65 @@ import DLRadioButton
 class AcceptViewController: UIViewController {
 
     @IBOutlet weak var allArgeeButton: DLRadioButton!
+    @IBOutlet weak var ServiceTermsButton: DLRadioButton!
+    @IBOutlet weak var dataPolicyButton: DLRadioButton!
+    @IBOutlet weak var locationBasedButton: DLRadioButton!
+    @IBOutlet weak var nextButton: UIButton! {
+        didSet {
+            nextButton.isEnabled = false
+            nextButton.alpha = 0.5
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         allArgeeButton.isMultipleSelectionEnabled = true
     }
 
     @IBAction func radioButtonTapped(_ sender: DLRadioButton) {
-        print(sender.currentTitle)
+        switch sender {
+        case allArgeeButton:
+            if allArgeeButton.isSelected {
+                ServiceTermsButton.isSelected = true
+                dataPolicyButton.isSelected = true
+                locationBasedButton.isSelected = true
+                nextButton.isEnabled = true
+                nextButton.alpha = 1
+            } else {
+                ServiceTermsButton.isSelected = false
+                dataPolicyButton.isSelected = false
+                locationBasedButton.isSelected = false
+                nextButton.isEnabled = false
+                nextButton.alpha = 0.5
+            }
+        case ServiceTermsButton:
+            if ServiceTermsButton.isSelected && dataPolicyButton.isSelected && locationBasedButton.isSelected {
+                nextButton.isEnabled = true
+                nextButton.alpha = 1
+            } else {
+                nextButton.isEnabled = false
+                nextButton.alpha = 0.5
+            }
+        case dataPolicyButton:
+            if ServiceTermsButton.isSelected && dataPolicyButton.isSelected && locationBasedButton.isSelected {
+                nextButton.isEnabled = true
+                nextButton.alpha = 1
+            } else {
+                nextButton.isEnabled = false
+                nextButton.alpha = 0.5
+            }
+        case locationBasedButton:
+            if ServiceTermsButton.isSelected && dataPolicyButton.isSelected && locationBasedButton.isSelected {
+                nextButton.isEnabled = true
+                nextButton.alpha = 1
+            } else {
+                nextButton.isEnabled = false
+                nextButton.alpha = 0.5
+            }
+        default:
+            break
+        }
+        
+        
     }
     @IBAction func nextButtonTapped(_ sender: UIButton) {
         let createNameViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CreateNameViewController") as! CreateNameViewController
