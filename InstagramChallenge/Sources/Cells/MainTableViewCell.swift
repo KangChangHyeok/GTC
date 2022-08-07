1c293f793a15cd574f662873fd7fe38445c1a299
KangChangHyeok <dpkyle97@gmail.com>
2022-08-07 15:42:00 +0900

    UI 작업
    - mainTableViewCell 레이아웃 작업 완료.

--- InstagramChallenge/Sources/Cells/MainTableViewCell.swift
+++ InstagramChallenge/Sources/Cells/MainTableViewCell.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import FSPagerView
 
 class MainTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var userProfileImage: UIImageView!
+    @IBOutlet weak var userIdLabel: UILabel!
+    @IBOutlet weak var viewMoreButton: UIButton!
+    @IBOutlet weak var userSlideImage: FSPagerView!
+    @IBOutlet weak var userSlideImageCount: FSPageControl!
+    @IBOutlet weak var likesCountLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
     }
